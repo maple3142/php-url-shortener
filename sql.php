20dad380662b0f7dba151843f8f7dc6b3b1e8269
maple3142 <kirby741852963@gmail.com>
2019-09-20 23:27:17 +0800

    update sql

--- sql.php
+++ sql.php
@@ -1,5 +1,10 @@
 <?php
-$conn=new PDO("sqlite:database.db");
+$conn = NULL;
+if (isset($_ENV['DB_CONN'])) {
+	$conn = new PDO($_ENV['DB_CONN'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+} else {
+	$conn = new PDO("sqlite:database.db");
+}
 $conn->exec("
 	CREATE TABLE IF NOT EXISTS links(
 		id varchar(10) NOT NULL,
